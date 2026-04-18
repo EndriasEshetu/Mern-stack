2c272a6b2a84dcd49ecdcf0b5abfbf5f6883dfa9
ENDRIASESHETU\hp <egataendrias@gmail.com>
2026-04-18 09:16:11 +0300

    feat(profile): add profile management with actions and reducer feat(dashboard): implement dashboard component with profile fetching feat(routing): create PrivateRoute component for protected routes

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import { Landing } from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Alert from "./components/layout/Alert";
+import Dashboard from "./components/dashboard/Dashboard";
+import PrivateRoute from "./components/routing/PrivateRoute";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
@@ -46,6 +48,7 @@ const App = () => {
                 </section>
               }
             />
+            <PrivateRoute path="/dashboard" component={Dashboard} />
           </Routes>
         </Fragment>
       </Router>
