4e04722c441248a6f8b5a4c186315315573b335c
ENDRIASESHETU\hp <egataendrias@gmail.com>
2026-04-21 18:23:45 +0300

    feat(profile): add ProfileExperience and ProfileEducation components to display user experience and education

---
+++ client/src/components/profile/ProfileEducation.js
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+import moment from "moment";
+
+const ProfileEducation = ({
+  education: { school, degree, fieldofstudy, current, to, from, description },
+}) => (
+  <div>
+    <h3 className="text-dark">{school}</h3>
+    <p>
+      {moment(from).format("YYYY/MM/DD")} -{" "}
+      {!to ? "Now" : moment(to).format("YYYY/MM/DD")}
+    </p>
+    <p>
+      <strong>Degree:</strong> {degree}
+    </p>
+    <p>
+      <strong>Field of Study:</strong> {fieldofstudy}
+    </p>
+    {description && (
+      <p>
+        <strong>Description:</strong> {description}
+      </p>
+    )}
+  </div>
+);
+
+ProfileEducation.propTypes = {
+  education: PropTypes.object.isRequired,
+};
+
+export default ProfileEducation;
---
+++ client/src/components/profile/ProfileExperience.js
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+import moment from "moment";
+const ProfileExperience = ({
+  experience: { company, title, location, current, to, from, description },
+}) => (
+  <div>
+    <h3 className="text-dark">{company}</h3>
+    <p>
+      {moment(from).format("YYYY/MM/DD")} -{" "}
+      {!to ? "Now" : moment(to).format("YYYY/MM/DD")}
+    </p>
+    <p>
+      <strong>Position:</strong> {title}
+    </p>
+    {description && (
+      <p>
+        <strong>Description:</strong> {description}
+      </p>
+    )}
+  </div>
+);
+
+ProfileExperience.propTypes = {
+  experience: PropTypes.object.isRequired,
+};
+
+export default ProfileExperience;
