a9dee3bc54b81d23865b7d41ea63ba864a600dd3
ENDRIASESHETU\hp <egataendrias@gmail.com>
2026-04-30 10:32:30 +0300

    fix(footer): update GitHub link to point to the correct repository

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -24,7 +24,7 @@ function Footer() {
 
         <div className="footer-links">
           <a
-            href="https://github.com/EndriasEshetu/Mortgage-calculator-web-app"
+            href="https://github.com/EndriasEshetu/Mern-stack"
             target="_blank"
             rel="noreferrer"
           >
